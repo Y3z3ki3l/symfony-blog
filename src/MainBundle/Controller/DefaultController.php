fe53fa5cd391595d014df46779e391e280cb5e32
Bruno Takahashi <bruno.kazuo90@gmail.com>
2017-03-05 11:29:49 -0300

    Added assetic bundle and configured the first page of blog

--- src/MainBundle/Controller/DefaultController.php
+++ src/MainBundle/Controller/DefaultController.php
@@ -6,17 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-
 class DefaultController extends Controller
 {
     /**
+     * This is the homepage of your blog
+     *
      * @Route("/")
      * @Template()
      */
     public function indexAction()
     {
-    	
-
     	return [];
     }
 }
